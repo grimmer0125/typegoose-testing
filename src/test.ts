8372c57d3ffb3eb5a1badaee951bc1dc35f530a7
hasezoey <hasezoey@gmail.com>
2020-08-01 22:14:31 +0200

    esm

--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,7 @@
 // NodeJS: 14.7.0
 // MongoDB: 4.2-bionic (Docker)
-import pkg from "@typegoose/typegoose"; // @typegoose/typegoose@7.3.1
+import { modelOptions, prop, getModelForClass } from "@typegoose/typegoose/lib/esm"; // @typegoose/typegoose@7.3.1
 import mongoose from "mongoose"; // mongoose@5.9.26 @types/mongoose@5.7.34
-const { getModelForClass, prop, modelOptions } = pkg;
 
 @modelOptions({ schemaOptions: { timestamps: true, strict: "throw", collection: "user" } })
 class User {
