e7da943e4f955e6e6e357a04119fa39d2f3be993
hasezoey <hasezoey@gmail.com>
2020-08-01 07:42:53 +0200

    unconfirmed

--- src/test.ts
+++ src/test.ts
@@ -1,20 +1,20 @@
 // NodeJS: 14.7.0
 // MongoDB: 4.2-bionic (Docker)
-import { getModelForClass, prop } from "@typegoose/typegoose"; // @typegoose/typegoose@7.3.1
-import * as mongoose from "mongoose"; // mongoose@5.9.26 @types/mongoose@5.7.34
+import pkg from "@typegoose/typegoose"; // @typegoose/typegoose@7.3.1
+import mongoose from "mongoose"; // mongoose@5.9.26 @types/mongoose@5.7.34
+const { getModelForClass, prop, modelOptions } = pkg;
 
+@modelOptions({ schemaOptions: { timestamps: true, strict: "throw", collection: "user" } })
 class User {
-  @prop()
-  public username?: string;
+  @prop({ required: true, index: true })
+  public userId!: number;
 }
 const UserModel = getModelForClass(User);
 
 (async () => {
-  await mongoose.connect(`mongodb://localhost:27017/`, { useNewUrlParser: true, dbName: "verifyMASTER", useCreateIndex: true, useUnifiedTopology: true });
+  await mongoose.connect(`mongodb://localhost:27017/`, { useNewUrlParser: true, dbName: "verify326again", useCreateIndex: true, useUnifiedTopology: true });
 
-  const doc = new UserModel({ username: "user1" });
 
-  console.log(doc);
 
   await mongoose.disconnect();
 })();
